dfa556a18701a19bd2494029f6dd06c304bf866f
Vishwa <1635@email.com>
2025-08-07 14:43:09 +0530

    prepare for gh-pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Routes, Route } from "react-router-dom";
 import './App.css'
-import Login from './pages/login';
-import Forgot from './pages/forgot';
+// import Login from './pages/login';
+// import Forgot from './pages/forgot';
 import Dashboard from './pages/Dashboard';
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
   return (
     <>
        <Routes>
-          <Route path='/' element={<Login/>} />
-          <Route path='/Forgot' element={<Forgot/>} />
-          <Route path='/Dashboard' element={<Dashboard/>} />
+          {/* <Route path='/' element={<Login/>} />
+          <Route path='/Forgot' element={<Forgot/>} /> */}
+          <Route path='/' element={<Dashboard/>} />
         </Routes>
     </>
   );
